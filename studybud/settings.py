5b19f77034b42c720e5292867fc266ad3ecc2d5d
devguru0112 <devguru0112@gmail.com>
2022-06-14 14:37:16 +0900

    learning about Python/Django

--- studybud/settings.py
+++ studybud/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'studybuddy',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
